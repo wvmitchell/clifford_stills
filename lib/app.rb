c2e17f5cc99790a51b9881cbf17234eda38becb7
Billy <navyosu@gmail.com>
2013-10-29 10:34:30 -0600

    Created paths to about the museum, about the building, about the collection. Made collection, building, and museum views work

--- lib/app.rb
+++ lib/app.rb
@@ -2,12 +2,16 @@ class ClyffordStillsApp < Sinatra::Base
 
   set :public, 'public'
 
-  get '/' do
+  get '/museum' do
     erb :index
   end
 
   get '/collection' do
     erb :collection
+  end
+
+  get '/museum/about-building' do 
+    erb :building
   end
 
 end
